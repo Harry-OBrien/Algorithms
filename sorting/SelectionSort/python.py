b13602bb57ddeb64ee5e16dc068614b4cd41f9e6
Harry O'Brien <34553387+Harry-OBrien@users.noreply.github.com>
2022-10-19 16:06:39 +0100

    Implement selection sort

--- sorting/SelectionSort/python.py
+++ sorting/SelectionSort/python.py
@@ -1,14 +1,24 @@
 import random
 
 def sort(arr):
-    pass
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+            
+        if i != min_idx:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
 
 def main():
     random.seed(0)
-    
+
     arr = [random.randint(0, 10) for _ in range(20)]
-    sorted_arr = sort(arr.copy)
-    print(sorted_arr)
+    print("Initial arr:", arr)
+    sorted_arr = sort(arr)
+    print("Sorted arr:", sorted_arr)
 
 if __name__ == "__main__":
     main()
