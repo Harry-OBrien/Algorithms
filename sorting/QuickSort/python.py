5b61185e55eada9e0b7b3a8fbfc61c2a78452dcb
Harry O'Brien <34553387+Harry-OBrien@users.noreply.github.com>
2022-10-19 11:44:13 +0100

    Implement quick sort

--- sorting/QuickSort/python.py
+++ sorting/QuickSort/python.py
@@ -1,14 +1,29 @@
 import random
 
 def sort(arr):
-    pass
+    if len(arr) <= 1:
+        return arr
+
+    # TODO: Optimise pivot point
+    pivot = arr[-1]
+
+    larger = []
+    smaller = []
+    for n in arr[:-1]:
+        if n < pivot:
+            smaller.append(n)
+        else:
+            larger.append(n)
+    
+    return sort(smaller) + [pivot] + sort(larger)
 
 def main():
     random.seed(0)
-    
+
     arr = [random.randint(0, 10) for _ in range(20)]
-    sorted_arr = sort(arr.copy)
-    print(sorted_arr)
+    print("Initial arr:", arr)
+    sorted_arr = sort(arr)
+    print("Sorted arr:", sorted_arr)
 
 if __name__ == "__main__":
     main()
