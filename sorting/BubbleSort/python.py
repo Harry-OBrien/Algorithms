f90904556642d5abada2db3ff13e535ec53fa09d
Harry O'Brien <34553387+Harry-OBrien@users.noreply.github.com>
2022-10-19 12:19:45 +0100

    Implement bubble sort

--- sorting/BubbleSort/python.py
+++ sorting/BubbleSort/python.py
@@ -1,14 +1,28 @@
 import random
 
 def sort(arr):
-    pass
+    sorted = False
+    
+    while not sorted:
+        sorted = True
+        for i, n in enumerate(arr[:-1]):
+            if n > arr[i+1]:
+                # swap
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+
+                sorted = False
+
+    return arr
 
 def main():
     random.seed(0)
-    
+
     arr = [random.randint(0, 10) for _ in range(20)]
-    sorted_arr = sort(arr.copy)
-    print(sorted_arr)
+    print("Initial arr:", arr)
+    sorted_arr = sort(arr)
+    print("Sorted arr:", sorted_arr)
 
 if __name__ == "__main__":
     main()
