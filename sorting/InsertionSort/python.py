da780a33db2e83ae085c3f7ef27b582fa03b6d8e
Harry O'Brien <34553387+Harry-OBrien@users.noreply.github.com>
2022-10-19 15:30:53 +0100

    Implement insertion sort

--- sorting/InsertionSort/python.py
+++ sorting/InsertionSort/python.py
@@ -1,14 +1,26 @@
 import random
 
-def sort(arr):
-    pass
+def sort(_arr):
+    arr = [x for x in _arr]
+    if len(arr) <= 1:
+        return arr
+
+    for i in range(1, len(arr)):
+        sort_val = arr[i]
+        
+        while arr[i-1] > sort_val and i > 0:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            i = i - 1
+
+    return arr
 
 def main():
     random.seed(0)
-    
+
     arr = [random.randint(0, 10) for _ in range(20)]
-    sorted_arr = sort(arr.copy)
-    print(sorted_arr)
+    print("Initial arr:", arr)
+    sorted_arr = sort(arr)
+    print("Sorted arr:", sorted_arr)
 
 if __name__ == "__main__":
     main()
